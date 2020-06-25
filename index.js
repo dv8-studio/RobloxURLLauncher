f526912efc03e6f9a12ce3bb2ae529a5c0a2252a
DevKurka <kampustelnik@outlook.com>
2020-06-25 19:03:06 +0200

    Init v1.4

---
+++ index.js
@@ -0,0 +1,31 @@
+const script = document.createElement('script')
+script.textContent = `
+const urlParams = new URLSearchParams(window.location.search)
+const options = { // Setup all possible actions and specify require arguments
+	followPlayerIntoGame: ['followUserId'],
+	editGameInStudio: ['editPlaceId'],
+	joinGameInstance: ['placeId', 'gameId'],
+	joinPrivateGame: ['privatePlaceId', ['privateId'], ['code']],
+	joinMultiplayerGame: ['placeId']
+}
+
+for (const action in options) { // Iterate actions
+	const data = options[action]
+	const input = []
+	for (const key of data) { // Fetches required parameters into table
+		const optional = Array.isArray(key)
+		const paramKey = optional ? key[0] : key
+
+		const value = urlParams.get(paramKey)
+		if (value != null) { input.push(value)
+		} else if (optional) { input.push(null)
+		}
+	}
+	if (input.length !== data.length) continue // Not enough values, check next action
+	
+	Roblox.GameLauncher[action](...input) // Execute
+	break
+}
+`
+window.document.body.appendChild(script)
+script.remove()
