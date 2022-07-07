a1f73f358ecb0cc10a523b259e80ec9e6053c4ed
DevKurka <42714242+DevKurka@users.noreply.github.com>
2022-07-07 13:53:47 +0200

    Patch for #5

--- index.js
+++ index.js
@@ -14,50 +14,52 @@ const minimumParams = {
 	joinPrivateGame: 2
 }
 
-for (const action in options) { // Iterate actions
-	const data = options[action]
-	const input = []
-	for (const key of data) { // Fetches required parameters into table
-		const optional = Array.isArray(key)
-		let paramKey = optional ? key[0] : key
-		if (!optional && typeof key === 'object') {
-			if (key.regex) {
-				const result = key.regex.exec(currentUrl)
-				if (result) {
-					input.push(result[1])
-					continue
+if (!urlParams.get('page')) { // Don't run if Roblox analytics headers are present (ref #5)
+	for (const action in options) { // Iterate actions
+		const data = options[action]
+		const input = []
+		for (const key of data) { // Fetches required parameters into table
+			const optional = Array.isArray(key)
+			let paramKey = optional ? key[0] : key
+			if (!optional && typeof key === 'object') {
+				if (key.regex) {
+					const result = key.regex.exec(currentUrl)
+					if (result) {
+						input.push(result[1])
+						continue
+					}
 				}
+				paramKey = key.param
 			}
-			paramKey = key.param
-		}
-		if (!paramKey) continue
+			if (!paramKey) continue
 
-		const value = urlParams.get(paramKey)
-		if (value != null) { input.push(value)
-		} else if (optional) { input.push(null)
+			const value = urlParams.get(paramKey)
+			if (value != null) { input.push(value)
+			} else if (optional) { input.push(null)
+			}
 		}
-	}
-	if (input.length !== data.length) continue // Not enough values, check next action
-	
-	if (minimumParams[action]) {
-		let nonNullParams = 0
-		for (const param of input) if (param != null) nonNullParams += 1
-		if (nonNullParams < minimumParams[action]) continue // Not enough values, check next action
-	}
-	
-	let attempt = 0
-	const interval = setInterval(() => {
-		attempt += 1
-		try {
-			Roblox.GameLauncher[action](...input) // Execute
-			clearInterval(interval)
-			if (!urlParams.get('dontClose')) document.body.classList.add('RBLX_URL_LAUNCHER_OPENED')
-		} catch(e) {
-			console.log("Couldn't run the game - ", e)
+		if (input.length !== data.length) continue // Not enough values, check next action
+
+		if (minimumParams[action]) {
+			let nonNullParams = 0
+			for (const param of input) if (param != null) nonNullParams += 1
+			if (nonNullParams < minimumParams[action]) continue // Not enough values, check next action
 		}
-		if (attempt > 3) clearInterval(interval) // Stop if exceeded the limit of attempts
-	}, 1000)
-	break
+
+		let attempt = 0
+		const interval = setInterval(() => {
+			attempt += 1
+			try {
+				Roblox.GameLauncher[action](...input) // Execute
+				clearInterval(interval)
+				if (!urlParams.get('dontClose')) document.body.classList.add('RBLX_URL_LAUNCHER_OPENED')
+			} catch(e) {
+				console.log("Couldn't run the game - ", e)
+			}
+			if (attempt > 3) clearInterval(interval) // Stop if exceeded the limit of attempts
+		}, 1000)
+		break
+	}
 }
 `
 window.document.body.appendChild(script)
