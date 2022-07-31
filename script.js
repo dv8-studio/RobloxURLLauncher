06ca82a158973428759290ecf15deb56777cc57f
Etheroit <42714242+Etheroit@users.noreply.github.com>
2022-07-31 23:00:13 +0200

    Update

---
+++ script.js
@@ -0,0 +1,61 @@
+const currentUrl = window.location.href
+const urlParams = new URLSearchParams(window.location.search)
+const options = { // Setup all possible actions and specify require arguments
+	followPlayerIntoGame: ['followUserId'],
+	editGameInStudio: ['editPlaceId'],
+	joinGameInstance: [{param: 'placeId', regex: /\/games\/(\d+)/}, 'gameId'],
+	joinPrivateGame: [{param: 'privatePlaceId', regex: /\/games\/(\d+)/}, ['privateId'], ['code']],
+	joinMultiplayerGame: ['placeId']
+}
+
+const minimumParams = {
+	joinPrivateGame: 2
+}
+
+if (!urlParams.get('page')) { // Don't run if Roblox analytics headers are present (ref #5)
+	for (const action in options) { // Iterate actions
+		const data = options[action]
+		const input = []
+		for (const key of data) { // Fetches required parameters into table
+			const optional = Array.isArray(key)
+			let paramKey = optional ? key[0] : key
+			if (!optional && typeof key === 'object') {
+				if (key.regex) {
+					const result = key.regex.exec(currentUrl)
+					if (result) {
+						input.push(result[1])
+						continue
+					}
+				}
+				paramKey = key.param
+			}
+			if (!paramKey) continue
+
+			const value = urlParams.get(paramKey)
+			if (value != null) { input.push(value)
+			} else if (optional) { input.push(null)
+			}
+		}
+		if (input.length !== data.length) continue // Not enough values, check next action
+
+		if (minimumParams[action]) {
+			let nonNullParams = 0
+			for (const param of input) if (param != null) nonNullParams += 1
+			if (nonNullParams < minimumParams[action]) continue // Not enough values, check next action
+		}
+
+		let attempt = 0
+		const interval = setInterval(() => {
+			attempt += 1
+			try {
+				Roblox.GameLauncher[action](...input) // Execute
+				clearInterval(interval)
+				if (!urlParams.get('dontClose')) document.body.classList.add('RBLX_URL_LAUNCHER_OPENED')
+			} catch(e) {
+				console.log("Couldn't run the game - ", e)
+			}
+			if (attempt > 3) clearInterval(interval) // Stop if exceeded the limit of attempts
+		}, 1000)
+		break
+	}
+}
